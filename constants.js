483a390d3b4c7fef033fdb9a3a71ae59760f25d8
Joseph Oun <josephoun14@gmail.com>
2021-06-20 21:06:34 +0300

    catch errors in effects

--- constants.js
+++ constants.js
@@ -3,5 +3,5 @@ module.exports = Object.freeze({
     HIGHEST_RANK: 5,
     LOWEST_RANK: 1,
     TOP_K: 5,
-    EXCLUDED_WORDS: ['on', 'the', 'a', 'an', 'of', 'by', 'in', 'as', 'is', 'to', 'if', 'and', 'or', 'that','was','were']
+    EXCLUDED_WORDS: ['on', 'the', 'a', 'an', 'of', 'by', 'in', 'as', 'is', 'to', 'if', 'and', 'or', 'that','was','were','for','are','with','from','it','at']
 });
--- router/api.js
+++ router/api.js
@@ -5,23 +5,24 @@ const ranker = require('../utils/ranker');
 
 const { TOP_K } = require('../constants');
 
-router.get('/rankWikiTopic',(req, res) => {
+router.get('/rankWikiTopic',async (req, res) => {
     let query = req.query.query;
     const url = `https://en.wikipedia.org/w/api.php`;
 
-    axios.get(url, {
-        params: {
-            action: "opensearch",
-            search: query
-        }
-    }).then((response) => {
+    try {
+        const response = await axios.get(url, {
+            params: {
+                action: "opensearch",
+                search: query
+            }
+        })
 
         // check if query has topics
         if (response.data && response.data[1] && response.data[1].length > 0) {
 
             const topic = response.data[1][0];
 
-            axios.get(`https://en.wikipedia.org/w/api.php`, {
+            const extractResponse = await axios.get(`https://en.wikipedia.org/w/api.php`, {
                 params: {
                     action: "query",
                     prop: "extracts",
@@ -29,41 +30,45 @@ router.get('/rankWikiTopic',(req, res) => {
                     titles: topic,
                     explaintext: ''
                 }
-            }).then(
-                (extractResponse) => {
-
-                    // validate response
-                    if (!extractResponse.data || !extractResponse.data.query) {
-                        res.status(404).send({success: false, data: []});
-                    }
-
-                    const extractDataQuery = extractResponse.data.query;
-
-                    // keep validating response
-                    if (!extractDataQuery.pages) {
-                        res.status(404).send({success: false, data: []});
-                    }
-
-                    // finally use ranker module to get top k ranked words
-                    const firstPage = Object.keys(extractDataQuery.pages)[0];
-                    const rankedKWords = ranker.topKOccurrences(extractDataQuery.pages[firstPage].extract, TOP_K);
-
-                    res.status(200).json({success: true, data: rankedKWords})
-                }
-            ).catch(err => {
-                // something went wrong while requesting from wikipedia
-                res.status(500).send({error: err})
             })
+
+            try {
+                const extractDataQuery = extractResponse.data.query;
+
+                // validate response
+                if (!extractResponse.data || !extractResponse.data.query) {
+                    res.status(404).send({success: false, data: [], message: 'No Topic Were Found'});
+                }
+
+                // keep validating response
+                if (!extractDataQuery.pages) {
+                    res.status(404).send({success: false, data: [], message: 'No Topic Were Found'});
+                }
+
+                // finally use ranker module to get top k ranked words
+                const firstPage = Object.keys(extractDataQuery.pages)[0];
+                const rankedKWords = ranker.topKOccurrences(extractDataQuery.pages[firstPage].extract, TOP_K);
+
+                // if no extracted text found return a message to the user
+                if (!rankedKWords || !rankedKWords.length) {
+                    res.status(200).send({success: false, data: [], message: 'No Topics Were Found'});
+                }
+                res.status(200).json({success: true, data: rankedKWords})
+
+            } catch (err) {
+                // something went wrong while requesting from wikipedia
+                res.status(500).send({error: err, message: err.message})
+            }
+
         } else {
             // no topics found on this query in wikipedia but no error found.
-            res.status(404).send({success: false, data: []});
+            res.status(200).send({success: false, data: [], message: 'No Topics Were Found'});
         }
-    }).catch(function (err) {
+    } catch (err) {
         // something went wrong while requesting from wikipedia
-        res.status(500).send({error: err})
-    });
+        res.status(500).send({error: err, message: err.message})
+    }
 
 });
-
 
 module.exports = router;
